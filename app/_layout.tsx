7e8a600a9593e8dc6f89b1df66e188503697f6da
Prabhakar1437 <prabhakarvatsking@gmail,com>
2025-09-02 02:23:47 +0530

    loading issues is coming

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-// app/_layout.tsx
 import React from 'react';
 import { Stack } from 'expo-router';
 import '../global.css';
@@ -6,9 +5,9 @@ import '../global.css';
 export default function RootLayout(): React.JSX.Element {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
       <Stack.Screen name="(auth)" />
       <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="index" />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -60,4 +60,3 @@ export default function Index() {
     </View>
   );
 }
-//tab/index.tsx
