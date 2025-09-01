e3a70c96a163ca5336f99957ccbe2221e2face3d
Prabhakar1437 <prabhakarvatsking@gmail,com>
2025-09-02 01:51:06 +0530

    Profile pic features added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+// app/_layout.tsx
 import React from 'react';
 import { Stack } from 'expo-router';
 import '../global.css';
@@ -5,6 +6,7 @@ import '../global.css';
 export default function RootLayout(): React.JSX.Element {
   return (
     <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
       <Stack.Screen name="(auth)" />
       <Stack.Screen name="(tabs)" />
     </Stack>
